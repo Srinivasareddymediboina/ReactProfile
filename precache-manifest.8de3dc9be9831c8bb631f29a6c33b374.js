57c040826b7a9989d65ab506aee489c2fefb669a
Harideep <hari.apssdc@gmail.com>
2020-04-29 14:35:07 +0530

    Updates

---
+++ precache-manifest.8de3dc9be9831c8bb631f29a6c33b374.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9764b707378f9ad0234df05ec745e57",
+    "url": "/Srinivasareddymediboina/ReactProfile.git/index.html"
+  },
+  {
+    "revision": "ced338299fd86df67ab5",
+    "url": "/Srinivasareddymediboina/ReactProfile.git/static/css/main.692e80e4.chunk.css"
+  },
+  {
+    "revision": "e36829ded9e21647f176",
+    "url": "/Srinivasareddymediboina/ReactProfile.git/static/js/2.9203a063.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Srinivasareddymediboina/ReactProfile.git/static/js/2.9203a063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ced338299fd86df67ab5",
+    "url": "/Srinivasareddymediboina/ReactProfile.git/static/js/main.1a10cf80.chunk.js"
+  },
+  {
+    "revision": "088c01bd12a1bf888872",
+    "url": "/Srinivasareddymediboina/ReactProfile.git/static/js/runtime-main.456058bc.js"
+  }
+]);
